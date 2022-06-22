88d4e63e5c79786c5a12a8de64d2c258a2c523c2
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2022-06-22 22:49:41 +0530

    access specifier, class vs structure

---
+++ access_spec.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+// notice how we a structure doesn't allow us to controll access levels
+// everything is public in a structure
+struct book
+{
+    int quantity;
+    std::string isbn;
+    std::string name;
+
+    book(int _quantity, std::string _isbn, std::string _name)
+    {
+        quantity = _quantity;
+        isbn = _isbn;
+        name = _name;
+    }
+
+    void read()
+    {
+        std::cout << name;
+    }
+};
+
+// default access specifier in a class is PRIVATE
+class person
+{
+    int uuid;
+
+public:
+    std::string name;
+
+    person(int _uuid, std::string _name)
+    {
+        uuid = _uuid;
+        name = _name;
+    }
+
+    int GetID()
+    {
+        return uuid;
+    }
+};
+
+int main()
+{
+    book odyssey = book(432, "bce", "an odyssey");
+    odyssey.read();
+    std::cout << "\n"
+              << odyssey.isbn
+              << " "
+              << odyssey.quantity
+              << "\n";
+
+    person somebody = person(1234, "somebody");
+    std::cout << somebody.name << "\n";
+    std::cout << somebody.GetID() << "\n";
+
+    return 0;
+}
