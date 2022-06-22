3af7448b32afe8d0e04a3d0941512addb394be3f
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2022-06-22 22:49:12 +0530

    smallest cpp program

---
+++ tiny.cpp
@@ -0,0 +1,6 @@
+// smallest, valid c++ program
+// doesn't do anything
+// only for learning purposes
+// an empty program in c++ is possible without including any preprocessor directives
+
+int main() {}
